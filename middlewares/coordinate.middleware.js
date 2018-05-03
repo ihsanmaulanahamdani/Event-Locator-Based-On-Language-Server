8310e995f9eb1b21f40821f7dc8b6d20480d4238
rg872 <gilang0293@gmail.com>
2018-05-04 00:46:22 +0700

    selesai membuat middleware buat get coordinate

---
+++ middlewares/coordinate.middleware.js
@@ -0,0 +1,15 @@
+function getCoordinate (req, res) {
+    let capital_name = req.capital
+    let mapApiKey = process.env.GOOGLE_MAP_API_KEY
+
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${capital_name}&key=${mapApiKey}`)
+    .then((response) => {
+        req.latitude = response.results[0].geometry.location.lat
+        req.longitude = response.results[0].geometry.location.lng
+        next()
+    })
+    .catch((err) => {
+        console.log(err)    
+    })
+
+}
