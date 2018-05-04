6918a4af75a34b722b6daf3ddaab1a0c7d98330d
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-05-04 09:54:09 +0700

    conflict fixed in hacktiv8

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose     = require('mongoose')
+const cors         = require('cors')
+mongoose.connect('mongodb://eventlocator:locatorevent@ds113200.mlab.com:13200/eventlocator')
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('success connect database')
+})
 
 var indexRouter = require('./routes/index');
 var languageRouter = require('./routes/language');
@@ -13,6 +22,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/language', languageRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
